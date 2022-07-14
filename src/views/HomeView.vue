dce67ee1937126301159b691e7457cc94a1ca15e
haku <no.114514@qq.com>
2022-07-14 10:19:00 +0800

    to solve tabbar bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="home">这里是首页</div>
+  <div class="home">
+    <div class="top-nav">
+      <div>昆明</div>
+      <div class="down-arrow"></div>
+    </div>
+    <div v-for="n in 100" :key="n">{{ n }}</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+<style scoped>
+.top-nav {
+  display: flex;
+  height: 1.5rem;
+}
+.down-arrow {
+  width: 0;
+  height: 0;
+  border: 5px solid black;
+  border-left: transparent;
+  border-bottom: transparent;
+  border-right: transparent;
+}
+</style>
